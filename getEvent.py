0d73d235a5ea0bfb2c9c1029ee304fcf681938c0
anant12 <ananttibrewal@gmail.com>
2017-04-10 13:36:04 -0500

    modified model, added data

--- getEvent.py
+++ getEvent.py
@@ -1,66 +1,71 @@
 import numpy as np
 from math import *
 
-def lonlat2Dis(lat1,lat2,lon1,lon2):
-	R = 6371000  #Mean earth radius
-	t1 = radians(lat1)
-	t2 = radians(lat2)
-	dt = radians(lat2 - lat1)
-	dl = radians(lon2 - lon1)
-	a = sin(dt / 2) * sin(dt / 2) + cos(t1) * cos(t2) * sin(dl / 2) * sin(dl / 2)
-	c = 2 * atan2(sqrt(a),sqrt(1-a))
-	return R * c
 
-def getDate(s,leap=False):
-	if leap:
-		months = [0,31,60,91,121,152,182,213,244,274,305,335]
-	else:
-		months = [0,31,29,90,120,151,181,212,243,273,304,334]
-	return months[int(s[5:7])-1]+int(s[8:10])
+def lonlat2Dis(lat1, lat2, lon1, lon2):
+    R = 6371000  # Mean earth radius
+    t1 = radians(lat1)
+    t2 = radians(lat2)
+    dt = radians(lat2 - lat1)
+    dl = radians(lon2 - lon1)
+    a = sin(dt / 2) * sin(dt / 2) + cos(t1) * cos(t2) * sin(dl / 2) * sin(dl / 2)
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    return R * c
 
-def toDate(date,leap=False):
-	if leap:
-		months = [0,31,60,91,121,152,182,213,244,274,305,335]
-	else:
-		months = [0,31,29,90,120,151,181,212,243,273,304,334]
-	for i in range(len(months)-1):
-		if date <= months[i+1]:
-			return str(i+1)+"."+str(date-months[i])
-	return str(len(months))+"."+str(date-months[-1])
+
+def getDate(s, leap=False):
+    if leap:
+        months = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
+    else:
+        months = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+    return months[int(s[5:7]) - 1] + int(s[8:10])
+
+
+def toDate(date, leap=False):
+    if leap:
+        months = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335]
+    else:
+        months = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+    for i in range(len(months) - 1):
+        if date <= months[i + 1]:
+            return str(i + 1) + "." + str(date - months[i])
+    return str(len(months)) + "." + str(date - months[-1])
+
 
 def getInfo(year):
-	res = []
-	with open(str(year)+"_lite.txt","r") as f:
-		line = f.readline()
-		line = f.readline()
-		while line != "":
-			infos = line.split("|")
-			line = f.readline()
-			date = infos[-10]
-			lat = infos[-3]
-			lon = infos[-2]
-			if date == "" or lat == "" or lon == "" or lat == "Unknown" or lon == "Unknown":
-				res.append([0,0,0])
-				continue
-			res.append([getDate(date,year % 4 == 0),float(lat),float(lon)])
-	f.close()
-	return res
+    res = []
+    with open(str(year) + "_lite.txt", "r") as f:
+        line = f.readline()
+        line = f.readline()
+        while line != "":
+            infos = line.split("|")
+            line = f.readline()
+            date = infos[-10]
+            lat = infos[-3]
+            lon = infos[-2]
+            if date == "" or lat == "" or lon == "" or lat == "Unknown" or lon == "Unknown":
+                res.append([0, 0, 0])
+                continue
+            res.append([getDate(date, year % 4 == 0), float(lat), float(lon)])
+    f.close()
+    return res
 
-def buildGraph(year,dateDiff,distDiff):
-	info = getInfo(year)
-	graph = {}
-	for i in range(len(info)):
-		graph[i] = []
-		if info[i][0] == 0:
-			continue
-		for j in range(i + 1, len(info)):
-			if info[j][0] == 0 or info[j][0] - info[i][0] > dateDiff:
-				continue
-			dist = lonlat2Dis(info[i][1],info[j][1],info[i][2],info[j][2])
-			if dist > distDiff:
-				continue
-			graph[i].append(j)
-	return info, graph
+
+def buildGraph(year, dateDiff, distDiff):
+    info = getInfo(year)
+    graph = {}
+    for i in range(len(info)):
+        graph[i] = []
+        if info[i][0] == 0:
+            continue
+        for j in range(i + 1, len(info)):
+            if info[j][0] == 0 or info[j][0] - info[i][0] > dateDiff:
+                continue
+            dist = lonlat2Dis(info[i][1], info[j][1], info[i][2], info[j][2])
+            if dist > distDiff:
+                continue
+            graph[i].append(j)
+    return info, graph
 
 # info, g = buildGraph(2015,1,500)
 # nextNonEmpty = 0
--- model.py
+++ model.py
@@ -7,52 +7,64 @@ from sklearn.linear_model import LinearRegression
 import pandas
 from getEvent import *
 from PIL import Image
+import csv
+import os
+import collections
 
-# ne = np.array(Image.open('1.tif'))
-# nw = np.array(Image.open('2.tif'))
-# se = np.array(Image.open('3.tif'))
-# sw = np.array(Image.open('4.tif'))
-# print(ne.shape)
+ne = np.array(Image.open('data/1.tif'))
+nw = np.array(Image.open('data/2.tif'))
+se = np.array(Image.open('data/3.tif'))
+sw = np.array(Image.open('data/4.tif'))
+print(ne.shape)
+print ne[0][0]
 # print(nw.shape)
 # print(se.shape)
 # print(sw.shape)
 # print(sw[0,0])
-#[ -1.70071534e+12   8.50358942e+11   8.50356394e+11   0.00000000e+00
+# [ -1.70071534e+12   8.50358942e+11   8.50356394e+11   0.00000000e+00
 #   8.30475438e-01]
-#19.6381824491
-f = pandas.read_csv("data/2015_lite.txt",'|')
-f = f.as_matrix(columns=f.columns[[-10,-3,-2]])
-f = f[f[:,1] != 'Unknown']
-f = f[f[:,0] != ""]
-f = f[~np.isnan(f[:,1:3].astype('float32')).any(axis=1)]
-for i in range(f.shape[0]):
-	f[i,0] = getDate(f[i,0])
-f = f.astype('float32')
+# 19.6381824491
+data311 = pandas.read_csv("data/2015_lite.txt", '|')
+data311 = data311.as_matrix(columns=data311.columns[[-10, -3, -2]])
+data311 = data311[data311[:, 1] != 'Unknown']
+data311 = data311[data311[:, 0] != ""]
+data311 = data311[~np.isnan(data311[:, 1:3].astype('float32')).any(axis=1)]
+for i in range(data311.shape[0]):
+    data311[i, 0] = getDate(data311[i, 0])  # convert month-day to number from 0-365
 
-ft = pandas.read_csv("data/2014_lite.txt",'|')
-ft = ft.as_matrix(columns=ft.columns[[-10,-3,-2]])
-ft = ft[ft[:,1] != 'Unknown']
-ft = ft[ft[:,0] != ""]
-ft = ft[~np.isnan(ft[:,1:3].astype('float32')).any(axis=1)]
+
+data311 = data311.astype('float32')
+
+ft = pandas.read_csv("data/2014_lite.txt", '|')
+ft = ft.as_matrix(columns=ft.columns[[-10, -3, -2]])
+ft = ft[ft[:, 1] != 'Unknown']
+ft = ft[ft[:, 0] != ""]
+ft = ft[~np.isnan(ft[:, 1:3].astype('float32')).any(axis=1)]
 for i in range(ft.shape[0]):
-	ft[i,0] = getDate(ft[i,0])
+    ft[i, 0] = getDate(ft[i, 0])
 ft = ft.astype('float32')
 
-maxLat = np.max(f[:,1])
-minLat = np.min(f[:,1])
-maxLon = np.max(f[:,2])
-minLon = np.min(f[:,2])
+maxLat = np.max(data311[:, 1])
+minLat = np.min(data311[:, 1])
+maxLon = np.max(data311[:, 2])
+minLon = np.min(data311[:, 2])
 print(maxLat)
 print(minLat)
 print(maxLon)
 print(minLon)
-exit()
+# exit()
+#gauges = pandas.read_csv("rainfall/gauges_loc.csv", ',')
+gauges = [(-95.178333, 29.517222), (-95.289167, 29.674167), (-95.396944, 29.775), (-95.093611, 29.876111),
+          (-95.141111, 29.916111), (-95.258333, 30.026389), (-95.124167, 30.145278), (-95.436111, 30.110278),
+          (-95.645833, 30.119722), (-95.428611, 30.035556), (-95.598333, 29.973333), (-95.808056, 29.95),
+          (-95.233056, 29.836944), (-95.417778, 29.956667), (-95.408333, 29.76), (-95.523333, 29.746667)]
+#locs = gauges['num'].as_matrix()
+locs = [120,340,520,720,750,760,790,1050,1070,1120,1160,1180,1620,1660]
 
-gauges = [(-95.178333,29.517222),(-95.289167,29.674167),(-95.396944,29.775),(-95.093611,29.876111),(-95.141111,29.916111),(-95.258333,30.026389),(-95.124167,30.145278),(-95.436111,30.110278),(-95.645833,30.119722),(-95.428611,30.035556),(-95.598333,29.973333),(-95.808056,29.95),(-95.233056,29.836944),(-95.417778,29.956667),(-95.408333,29.76),(-95.523333,29.746667)]
-locs = [120,340,520,720,750,760,790,1050,1070,1120,1160,1180,1620,1660,2240,2260]
+#gauges = gauges[['LONGITUDE', 'LATITUDE']].as_matrix()
 
-radius = 0.12
-num = 25
+radius = 0.03
+num = 100
 
 # rainfall = []
 # for loc in locs:
@@ -91,85 +103,115 @@ num = 25
 # 	print(np.sum((pred-density)**2)/num/num)
 
 # exit()
-
 rainfall = []
 for loc in locs:
-	rainfall.append(np.genfromtxt(str(loc)+".csv"))
+    try:
+        rainfall.append(np.genfromtxt('rainfall/' + str(loc) + ".csv"))
+    except IOError:
+        continue
 rainfall = np.asarray(rainfall)
+
+gauge_data = collections.defaultdict(list)
+for filename in os.listdir('rainfall/raindata_scrape'):
+    gauge = filename.split("-")[0]
+    print filename, gauge
+    f = csv.reader(filename)
+    for row in f:
+        gauge_data[gauge].append(row)
+
 
 clf = LinearRegression()
 features = []
 values = []
-for i in range(31):
-	print(i)
-	X = np.linspace(minLat,maxLat,num)
-	Y = np.linspace(minLon,maxLon,num)
-	density = np.zeros((num,num))
-	rdensity = np.zeros((num,num))
-	zero = True
-	for xi in range(num):
-		for yi in range(num):
-			for row in f:
-				if (row[1] - X[xi])**2 + (row[2] - Y[yi])**2 < radius**2:
-					if row[0] == 120 + i:
-						density[xi,yi] += 1
-						zero = False
-			for j in range(len(locs)):
-				if (gauges[j][1] - X[xi])**2 + (gauges[j][0] - Y[yi])**2 < radius**2:
-					rdensity[xi,yi] += rainfall[j,i]
-					if rainfall[j,i] != 0:
-						zero = False
-			if density[xi,yi] != 0 and rdensity[xi,yi] != 0:
-				cover = []
-				if yi < num / 2 and xi < num / 2:
-					cover = se[8112 / num * 2 * yi, 8112 / num * 2 * xi]
-				elif yi >= num / 2 and xi < num / 2:
-					cover = sw[4054 / num * 2 * (yi-num/2), 8112 / num * 2 * xi]
-				elif yi < num / 2 and xi >= num / 2:
-					cover = ne[8112 / num * 2 * yi, 8112 / num * 2 * (xi-num/2)]
-				else:
-					cover = nw[8112 / num * 2 * (yi-num/2), 8112 / num * 2 * (xi-num/2)]
-				features.append(np.asarray([cover[0]/255.0,cover[1]/255.0,cover[2]/255.0,cover[3]/255.0,rdensity[xi,yi]]))
-				values.append(density[xi,yi])
-	if not zero:
-		print("plot: "+str(i))
-		plt.figure()
-		plt.imshow(density,extent=(minLat,maxLat,minLon,maxLon),cmap=cm.gist_rainbow)
-		plt.figure()
-		plt.imshow(rdensity,extent=(minLat,maxLat,minLon,maxLon),cmap=cm.gist_rainbow)
+coords = []
+
+for i in range(31):  # days of month
+    print(i)
+    X = np.linspace(minLat, maxLat, num)
+    Y = np.linspace(minLon, maxLon, num)
+    hdensity = np.zeros((num, num))
+    rdensity = np.zeros((num, num))
+    zero = True
+    # divide into lat long sub-zones
+    for xi in range(num):
+        for yi in range(num):
+            for row in data311: # adding 311 data
+                # 311 call is within radius of current location
+                if (row[1] - X[xi]) ** 2 + (row[2] - Y[yi]) ** 2 < radius ** 2:
+                    if row[0] == 120 + i:
+                    #if row[0] % 31 == i:
+                        #print row[0]
+                        hdensity[xi, yi] += 1
+                        zero = False
+
+            for j in range(len(locs)): # adding rainfall data
+                # guage is within radius of current location
+                if (gauges[j][1] - X[xi]) ** 2 + (gauges[j][0] - Y[yi]) ** 2 < radius ** 2:
+                    rdensity[xi, yi] += rainfall[j, i]
+                    if rainfall[j, i] != 0:
+                        zero = False
+            if hdensity[xi, yi] != 0 and rdensity[xi, yi] != 0:  # checks which submatrix it is - which direction
+                cover = []
+                if yi < num / 2 and xi < num / 2:
+                    cover = se[8112 / num * 2 * yi, 8112 / num * 2 * xi]
+                elif yi >= num / 2 and xi < num / 2:
+                    cover = sw[4054 / num * 2 * (yi - num / 2), 8112 / num * 2 * xi]
+                elif yi < num / 2 and xi >= num / 2:
+                    cover = ne[8112 / num * 2 * yi, 8112 / num * 2 * (xi - num / 2)]
+                else:
+                    cover = nw[8112 / num * 2 * (yi - num / 2), 8112 / num * 2 * (xi - num / 2)]
+                #features.append(np.asarray(
+                #    [cover[0] / 255.0, cover[1] / 255.0, cover[2] / 255.0, cover[3] / 255.0, hdensity[xi, yi]]))
+                #features.append(np.asarray([cover[0] / 255.0, hdensity[xi, yi]]))
+                #features.append(np.asarray([cover[0] / 255.0]))
+                features.append(np.asarray([rdensity[xi, yi]]))
+                #values.append(rdensity[xi, yi])
+                values.append(hdensity[xi, yi])
+                coords.append((X[xi], Y[yi], i))
+
+    if not zero:
+        '''print("plot: " + str(i))
+        plt.figure()
+        plt.imshow(density, extent=(minLat, maxLat, minLon, maxLon), cmap=cm.gist_rainbow)
+        plt.figure()
+        plt.imshow(rdensity, extent=(minLat, maxLat, minLon, maxLon), cmap=cm.gist_rainbow)
+        plt.show()'''
 print(features)
 print("----------")
 print(values)
-clf.fit(np.asarray(features),np.asarray(values))
+clf.fit(np.asarray(features), np.asarray(values))
 plt.show()
 print(clf.coef_)
 print(clf.intercept_)
-exit()
+plt.scatter(features, values,  color='black')
+plt.plot(features, clf.predict(features), color='blue', linewidth=3)
+plt.show()
 
-
-X = np.linspace(minLat,maxLat,num)
-Y = np.linspace(minLon,maxLon,num)
+#exit()
+'''
+X = np.linspace(minLat, maxLat, num)
+Y = np.linspace(minLon, maxLon, num)
 # density = [np.zeros((num,num)),np.zeros((num,num)),np.zeros((num,num)),np.zeros((num,num)),np.zeros((num,num)),np.zeros((num,num))]
-density = np.zeros((num,num))
+density = np.zeros((num, num))
 for xi in range(num):
-	print(xi)
-	for yi in range(num):
-		for row in f:
-			if (row[1] - X[xi])**2 + (row[2] - Y[yi])**2 < radius**2:
-				if row[0] == 145:
-					density[xi,yi] += 1
-				# if row[0] < 60:
-				# 	density[0][xi,yi] += 1
-				# elif row[0] < 121:
-				# 	density[1][xi,yi] += 1
-				# elif row[0] < 182:
-				# 	density[2][xi,yi] += 1
-				# elif row[0] < 244:
-				# 	density[3][xi,yi] += 1
-				# elif row[0] < 305:
-				# 	density[4][xi,yi] += 1
-				# else:
-				# 	density[5][xi,yi] += 1
+    print(xi)
+    for yi in range(num):
+        for row in data311:
+            if (row[1] - X[xi]) ** 2 + (row[2] - Y[yi]) ** 2 < radius ** 2:
+                if row[0] == 145:
+                    density[xi, yi] += 1
+                    # if row[0] < 60:
+                    # 	density[0][xi,yi] += 1
+                    # elif row[0] < 121:
+                    # 	density[1][xi,yi] += 1
+                    # elif row[0] < 182:
+                    # 	density[2][xi,yi] += 1
+                    # elif row[0] < 244:
+                    # 	density[3][xi,yi] += 1
+                    # elif row[0] < 305:
+                    # 	density[4][xi,yi] += 1
+                    # else:
+                    # 	density[5][xi,yi] += 1
 
 # wrong = 0
 # for row in ft:
@@ -211,5 +253,6 @@ print(X[1])
 # 	plt.figure()
 # 	plt.imshow(density[i],extent=(minLat,maxLat,minLon,maxLon),cmap=cm.gist_rainbow)
 plt.figure()
-plt.imshow(density,extent=(minLat,maxLat,minLon,maxLon),cmap=cm.gist_rainbow)
+plt.imshow(density, extent=(minLat, maxLat, minLon, maxLon), cmap=cm.gist_rainbow)
 plt.show()
+'''
